795b383aba5777fef5c4cc8d34f3dd2c4e859211
lethanhan01 <An.LT235631@sis.hust.edu.vn>
2026-01-21 04:29:21 +0700

    hehe

--- src/components/showcase/VerticalNavbar.tsx
+++ src/components/showcase/VerticalNavbar.tsx
@@ -6,7 +6,6 @@ export interface VerticalNavbarProps {}
 
 const VerticalNavbar: React.FC<VerticalNavbarProps> = (props) => {
     const location = useLocation();
-    const [projectsExpanded, setProjectsExpanded] = useState(false);
     const [isHome, setIsHome] = useState(false);
 
     const navigate = useNavigate();
@@ -15,11 +14,6 @@ const VerticalNavbar: React.FC<VerticalNavbarProps> = (props) => {
     };
 
     useEffect(() => {
-        if (location.pathname.includes('/projects')) {
-            setProjectsExpanded(true);
-        } else {
-            setProjectsExpanded(false);
-        }
         if (location.pathname === '/') {
             setIsHome(true);
         } else {
